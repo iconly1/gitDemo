bdaa2f1f989901cdb1a37671255e7fa2638d2247
iconly1 <iconly1@gmail.com>
2020-08-01 12:45:54 -0400

    int 1st.java

---
+++ src/JavaLove.java
@@ -0,0 +1,11 @@
+public class JavaLove {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        if (a+b>= 50){
+            System.out.println("Profit");
+        }else{
+            System.out.println("dont sell");
+        }
+    }
+}
