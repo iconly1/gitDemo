aa45a81612011b4017442a47cd651874cc7a2033
iconly1 <iconly1@gmail.com>
2020-08-01 12:13:31 -0400

    Happy B20 Practice log 2.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -2,5 +2,6 @@ public class HappyB20 {
     public static void main(String[] args) {
         System.out.println("B20 is always happy");
         System.out.println("I love Git");
+        System.out.println("This is fun");
     }
 }
